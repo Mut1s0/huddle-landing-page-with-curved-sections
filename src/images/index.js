4ad642c50e66f89494ab34171904d8a735bb5e3a
Allan Musyoka <musyokaallan96@gmail.com>
2024-05-23 17:26:49 +0300

    Added images

---
+++ src/images/index.js
@@ -0,0 +1,43 @@
+import iconCommunities from "./icon-communities.svg";
+import iconEmail from "./icon-email.svg";
+import iconMesages from "./icon-messages.svg";
+import iconPhone from "./icon-phone.svg";
+import illustartionFlowingConversation from "./illustration-flowing-conversation.svg";
+import illustartionGrowTogether from "./illustration-grow-together.svg";
+import illustartionYourUsers from "./illustration-your-users.svg";
+import screenMockups from "./illustration-screen-mockups.svg";
+
+// Desktop Images
+import bgFooterTopDesktop from "./bg-footer-top-desktop.svg";
+import bgSectionBottomDesktop1 from "./bg-section-bottom-desktop-1.svg";
+import bgSectionBottomDesktop2 from "./bg-section-bottom-desktop-2.svg";
+import bgSectionTopDesktop1 from "./bg-section-top-desktop-1.svg";
+import bgSectionTopDesktop2 from "./bg-section-top-desktop-2.svg";
+
+// Mobile Images
+import bgFooterTopMobile from "./bg-footer-top-mobile.svg";
+import bgSectionBottomMobile1 from "./bg-section-bottom-mobile-1.svg";
+import bgSectionBottomMobile2 from "./bg-section-bottom-mobile-2.svg";
+import bgSectionTopMobile1 from "./bg-section-top-mobile-1.svg";
+import bgSectionTopMobile2 from "./bg-section-top-mobile-2.svg";
+
+export {
+    iconCommunities,
+    iconEmail,
+    iconMesages,
+    iconPhone,
+    illustartionFlowingConversation,
+    illustartionGrowTogether,
+    illustartionYourUsers,
+    screenMockups,
+    bgFooterTopDesktop,
+    bgSectionBottomDesktop1,
+    bgSectionBottomDesktop2,
+    bgSectionTopDesktop1,
+    bgSectionTopDesktop2,
+    bgFooterTopMobile,
+    bgSectionBottomMobile1,
+    bgSectionBottomMobile2,
+    bgSectionTopMobile1,
+    bgSectionTopMobile2
+}
