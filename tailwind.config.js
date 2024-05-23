c56e013ee4a862849142cac7ca5db88b949551b6
Allan Musyoka <musyokaallan96@gmail.com>
2024-05-23 16:48:41 +0300

    Added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+
+        // Primary
+
+        pink: "hsl( 172, 67%, 45% )",
+        lightPink: "hsl( 321, 100%, 78% )",
+        lightRed: "hsl( 0, 100%, 63% )",
+
+        // Neutral
+
+        veryDarkCyan: "hsl( 192, 100%, 9% )",
+        veryPaleBlue: "hsl( 207, 100%, 98% )"
+
+      },
+      fontFamily: {
+        openSans: [ "Open Sans", "sans-serif" ],
+        poppins: [ "Poppins", "sans-serif" ]
+      },
+      fontWeight: {
+        poppinsBold: "700",
+        openSansRegular: "400",
+        openSansSemiBold: "600",
+        openSansBold: "700"
+      },
+    },
+    screens: {
+      xs: "344px",
+      ss: "375px",
+      sm: "425px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px"
+    }
+  },
+  plugins: [],
+}
+
